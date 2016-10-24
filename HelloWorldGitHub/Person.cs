0747e769c6d9afe2d9995ada42679effbc6f0d53
drago87 <dr.drago.87@hotmail.com>
2016-10-24 10:03:42 +0200

    Added a Class

---
+++ HelloWorldGitHub/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldGitHub
+{
+    class Person
+    {
+        //Bellongs to Feature1.1
+    }
+}
